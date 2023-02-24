3e68e683f8f80d2caf83307f45bda3ea7fd7455b
jdrbordon <104445711+jdrbordon@users.noreply.github.com>
2023-02-24 13:26:54 +0000

    Update build_lse_mechanics_bem_harela.f90
    Add handling of be bodyload elements belonging to symmetry planes

--- src/build_lse_mechanics_bem_harela.f90
+++ src/build_lse_mechanics_bem_harela.f90
@@ -1535,6 +1535,13 @@ subroutine build_lse_mechanics_bem_harela_bl(omega,kr,sb_int,se_int,se_int_n_nod
   complex(kind=real64), allocatable :: g(:,:,:), gt(:,:,:), gp(:,:,:), l(:,:,:)
   ! Multiplier for Dual Burton & Miller formulation
   real(kind=real64)      :: alpha
+  ! Symmetry plane configuration for the current element
+  integer                :: se_n_symplanes
+  integer                :: se_n_symelements
+  real(kind=real64)      :: se_symplane_m(3,3)
+  real(kind=real64)      :: se_symplane_s(3)
+  real(kind=real64)      :: se_symplane_t(3,3)
+  real(kind=real64)      :: se_symplane_r(3,3)
   ! Associated with symmetry
   real(kind=real64)      :: symconf_m(problem%n), symconf_t(problem%n), symconf_r(problem%n), symconf_s
   logical                :: reversed
@@ -1582,12 +1589,15 @@ subroutine build_lse_mechanics_bem_harela_bl(omega,kr,sb_int,se_int,se_int_n_nod
   ! Copy the incident field over the element
   u_inc=element(se_int)%incident_c(1:problem%n,:,1)
 
+  ! ACTIVE SYMMETRY PLANES FOR THE CURRENT ELEMENT
+  call build_symplane_bodyload_elements(se_int,se_n_symplanes,se_n_symelements,se_symplane_m,se_symplane_s,se_symplane_t,se_symplane_r)
+
   !
   ! Loop through SYMMETRICAL ELEMENTS for INTEGRATION
   !
-  do ks=1,n_symelements
+  do ks=1,se_n_symelements
     ! SYMMETRY SETUP
-    call fbem_symmetry_multipliers(ks,problem%n,n_symplanes,symplane_m,symplane_s,symplane_t,symplane_r,&
+    call fbem_symmetry_multipliers(ks,problem%n,se_n_symplanes,se_symplane_m,se_symplane_s,se_symplane_t,se_symplane_r,&
                                    symconf_m,symconf_s,symconf_t,symconf_r,reversed)
     do kn=1,se_int_n_nodes
       se_int_data%x(:,kn)=symconf_m*element(se_int)%x_gn(:,kn)
